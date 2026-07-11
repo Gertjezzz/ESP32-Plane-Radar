87b31ddb621e23f758c9c13a0f4ba96079493b24
Gertjezzz <Gertjezzz@hotmail.com>
2026-07-11 13:06:01 +0200

    Create radar_theme.cpp

---
+++ src/ui/radar_theme.cpp
@@ -0,0 +1,16 @@
+#include "ui/radar_theme.h"
+#include <TFT_eSPI.h>
+
+namespace ui::radar {
+
+Theme kRadarTheme = {
+  TFT_NAVY,
+  TFT_DARKGREEN,
+  TFT_DARKGREEN,
+  TFT_RED,
+  TFT_MAGENTA,
+  TFT_ORANGE,
+  TFT_YELLOW
+};
+
+}  // namespace ui::radar
